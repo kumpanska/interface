01f75f1d6aad40d3b1f825fd1754e7e43b936c58
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-12-09 23:01:47 +0200

    add System.Numeric and class MyFrac with fields and constructor

---
+++ taskinterface/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace taskinterface
+{
+    class MyFrac
+    {
+        private BigInteger nom;
+        private BigInteger denom;
+        public MyFrac(BigInteger nom, BigInteger denom)
+        {
+            if (denom == 0)
+            {
+                throw new DivideByZeroException("Denominator can't be zero");
+            }
+            this.nom = nom;
+            this.denom = denom;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
