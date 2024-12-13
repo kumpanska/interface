bc4abfac2e745a886c8cbc9f4f7f08c93370fe69
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-12-13 23:06:39 +0200

    add new tests

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -17,5 +17,37 @@ namespace TestProject1
             var result = complex1.Multiply(complex2);
             Assert.AreEqual("-5+10i", result.ToString());
         }
+        [TestMethod]
+        public void TestMyComplex_Divide()
+        {
+            var complex1 = new MyComplex(1, 2);
+            var complex2 = new MyComplex(1, 3);
+            var result = complex1.Divide(complex2);
+            Assert.AreEqual("0,7-0,1i", result.ToString());
+        }
+        [TestMethod]
+        public void TestMyComplex_Substract()
+        {
+            var complex1 = new MyComplex(5, 4);
+            var complex2 = new MyComplex(3, 7);
+            var result = complex1.Subtract(complex2);
+            Assert.AreEqual("2-3i", result.ToString());
+        }
+        [TestMethod]
+        public void TestMyComplex_Addition()
+        {
+            var complex1 = new MyComplex(10, 9);
+            var complex2 = new MyComplex(2, 7);
+            var result = complex1.Subtract(complex2);
+            Assert.AreEqual("8+2i", result.ToString());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestMyComplex_ExceptionDivideByZero()
+        {
+            var complex1 = new MyComplex(-1, 2);
+            var complex2 = new MyComplex(0, 0);
+            var result = complex1.Divide(complex2);
+        }
     }
 }
