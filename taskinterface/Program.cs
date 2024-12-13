d9beafce2b34ab4c803f2909f5af12a3d310db71
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-12-13 11:04:21 +0200

    add change in main and add entry point

--- taskinterface/Program.cs
+++ taskinterface/Program.cs
@@ -118,6 +118,33 @@ namespace taskinterface
     {
         static void Main(string[] args)
         {
+            TestAPlusBSquare(new MyFrac(1, 3), new MyFrac(1, 6));
+            TestAPlusBSquare(new MyComplex(1, 3), new MyComplex(1, 6));
+            Console.ReadKey();
+        }
+        static void TestAPlusBSquare<T>(T a, T b) where T : IMyNumber<T>
+        {
+            Console.WriteLine("=== Starting testing (a+b)^2=a^2+2ab+b^2 with a = " + a + ", b = " + b + " ===");
+            T aPlusB = a.Add(b);
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("(a + b) = " + aPlusB);
+            Console.WriteLine("(a+b)^2 = " + aPlusB.Multiply(aPlusB));
+            Console.WriteLine(" = = = ");
+            T curr = a.Multiply(a);
+            Console.WriteLine("a^2 = " + curr);
+            T wholeRightPart = curr;
+            curr = a.Multiply(b); // ab
+            curr = curr.Add(curr); // ab + ab = 2ab
+                                   // I'm not sure how to create constant factor "2" in more elegant way,
+                                   // without knowing how IMyNumber is implemented
+            Console.WriteLine("2*a*b = " + curr);
+            wholeRightPart = wholeRightPart.Add(curr);
+            curr = b.Multiply(b);
+            Console.WriteLine("b^2 = " + curr);
+            wholeRightPart = wholeRightPart.Add(curr);
+            Console.WriteLine("a^2+2ab+b^2 = " + wholeRightPart);
+            Console.WriteLine("=== Finishing testing (a+b)^2=a^2+2ab+b^2 with a = " + a + ", b = " + b + " ===");
         }
     }
 }
